54294ae7594a89d36843691c498355c990155ccf
dhanie <romadani.code@gmail.com>
2025-01-02 21:16:12 +0700

    get data

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::latest()->paginate(5);
+
+        return new PostResource(true, 'List Data Post', $posts);
+    }
 }
